47e0a82a5e015cf9c7b788aad34a9a511a847b25
pinguinson <nikita.penguin@gmail.com>
2017-05-19 09:18:39 +0300

    moved service host/port to application.conf

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -2,21 +2,26 @@ import actors.MainActor
 import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 import http.HttpService
-
-import scala.concurrent.ExecutionContext
 
 /**
   * Created by pinguinson on 3/29/2017.
   */
 object MainApp extends App {
 
+  val config = ConfigFactory.load()
+  val host = config.getString("http.host")
+  val port = config.getInt("http.port")
+
   implicit val system = ActorSystem("Shows")
-  implicit val executor: ExecutionContext = system.dispatcher
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val materializer = ActorMaterializer()
+
+  implicit val executionContext = system.dispatcher
   system.actorOf(Props[MainActor], "mainActor")
 
   val httpService = new HttpService
 
-  Http().bindAndHandle(httpService.route, "localhost", 1337)
+  Http().bindAndHandle(httpService.route, host, port)
+  println(s"Listening at $host:$port")
 }
